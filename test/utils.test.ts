20c6339d6a083ba3f86af56bb693a449de7f8c75
Marc Littlemore <marc.littlemore@netlify.com>
2022-12-05 10:18:49 +0000

    test: add test for missing transforms + refactor with correct paths

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,10 +2,10 @@ import test from 'ava'
 import {parseURL} from 'ufo'
 import {decodeBase64Params, doPatternsMatchUrl, RemotePattern} from '../src/utils'
 
-const encodeUrlAndTransforms = (url: string, transforms: string) : string => {
+const encodeUrlAndTransforms = (url: string = '', transforms: string = '', extension: string = '') : string => {
   const encodedUrl = Buffer.from(url).toString('base64')
   const encodedTransforms = Buffer.from(transforms).toString('base64')
-  return `${encodedUrl}/${encodedTransforms}.ext`
+  return `${encodedUrl}/${encodedTransforms}${extension}`
 }
 
 test('decodeBase64Params: returns expected response if no url is present', (t) => {
@@ -22,23 +22,35 @@ test('decodeBase64Params: returns expected response if no transform exists in pa
   const expectedResponse = {
     error: 'Bad Request'
   }
+
+  const encodedUrl = encodeUrlAndTransforms('/images/image.jpg', '')
   
-  const response = decodeBase64Params('fakeUrl/')
+  const response = decodeBase64Params(encodedUrl)
   
   t.deepEqual(response, expectedResponse)
 })
 
-// TODO: Can id and transform ever become falsy when base64 decoding if we're passing in a string?
-// Need tests for these if so
+test("decodeBase64Params: returns expected response if transform doesn't contain any transforms", (t) => {
+  const expectedId = '/images/';
+  const expectedResponse = {
+    error: 'Bad Request'
+  }
+  
+  const encodedUrl = encodeUrlAndTransforms(expectedId, '', '.ext')
+  
+  const response = decodeBase64Params(encodedUrl)
+  
+  t.deepEqual(response, expectedResponse)
+})
 
 test('decodeBase64Params: returns expected response if transform contains w and h', (t) => {
-  const expectedId = 'https://fake.url';
+  const expectedId = '/images/image.jpg';
   const expectedResponse = {
     id: expectedId,
     modifiers: 's_100x200'
   }
 
-  const encodedUrl = encodeUrlAndTransforms(expectedId, 'w=100&h=200')
+  const encodedUrl = encodeUrlAndTransforms(expectedId, 'w=100&h=200', '.jpg')
   
   const response = decodeBase64Params(encodedUrl)
   
@@ -46,13 +58,13 @@ test('decodeBase64Params: returns expected response if transform contains w and 
 })
 
 test('decodeBase64Params: returns expected response if transform contains w but no h', (t) => {
-  const expectedId = 'https://fake.url';
+  const expectedId = '/images/image.jpg';
   const expectedResponse = {
     id: expectedId,
     modifiers: 'w_100'
   }
 
-  const encodedUrl = encodeUrlAndTransforms(expectedId, 'w=100')
+  const encodedUrl = encodeUrlAndTransforms(expectedId, 'w=100', '.jpg')
   
   const response = decodeBase64Params(encodedUrl)
   
@@ -60,13 +72,13 @@ test('decodeBase64Params: returns expected response if transform contains w but 
 })
 
 test('decodeBase64Params: returns expected response if transform contains h but no w', (t) => {
-  const expectedId = 'https://fake.url';
+  const expectedId = '/images/image.jpg';
   const expectedResponse = {
     id: expectedId,
     modifiers: 'h_200'
   }
 
-  const encodedUrl = encodeUrlAndTransforms(expectedId, 'h=200')
+  const encodedUrl = encodeUrlAndTransforms(expectedId, 'h=200', '.jpg')
   
   const response = decodeBase64Params(encodedUrl)
   
@@ -74,13 +86,13 @@ test('decodeBase64Params: returns expected response if transform contains h but 
 })
 
 test('decodeBase64Params: returns expected response if transform contains fm', (t) => {
-  const expectedId = 'https://fake.url';
+  const expectedId = '/images/image.jpg';
   const expectedResponse = {
     id: expectedId,
     modifiers: 'f_10'
   }
 
-  const encodedUrl = encodeUrlAndTransforms(expectedId, 'fm=10')
+  const encodedUrl = encodeUrlAndTransforms(expectedId, 'fm=10', '.jpg')
   
   const response = decodeBase64Params(encodedUrl)
   
@@ -88,13 +100,13 @@ test('decodeBase64Params: returns expected response if transform contains fm', (
 })
 
 test('decodeBase64Params: returns expected response if transform contains q', (t) => {
-  const expectedId = 'https://fake.url';
+  const expectedId = '/images/image.jpg';
   const expectedResponse = {
     id: expectedId,
     modifiers: 'q_101'
   }
 
-  const encodedUrl = encodeUrlAndTransforms(expectedId, 'q=101')
+  const encodedUrl = encodeUrlAndTransforms(expectedId, 'q=101', '.jpg')
   
   const response = decodeBase64Params(encodedUrl)
   
@@ -102,13 +114,13 @@ test('decodeBase64Params: returns expected response if transform contains q', (t
 })
 
 test('decodeBase64Params: returns expected response if transform contains pos', (t) => {
-  const expectedId = 'https://fake.url';
+  const expectedId = '/images/image.jpg';
   const expectedResponse = {
     id: expectedId,
     modifiers: 'crop_10 20'
   }
 
-  const encodedUrl = encodeUrlAndTransforms(expectedId, 'pos=10,20')
+  const encodedUrl = encodeUrlAndTransforms(expectedId, 'pos=10,20', '.jpg')
   
   const response = decodeBase64Params(encodedUrl)
   
@@ -116,13 +128,13 @@ test('decodeBase64Params: returns expected response if transform contains pos', 
 })
 
 test('decodeBase64Params: returns expected response if transform contains multiple valid props', (t) => {
-  const expectedId = 'https://fake.url';
+  const expectedId = '/images/image.jpg';
   const expectedResponse = {
     id: expectedId,
     modifiers: 's_100x200,f_101,crop_10 20,q_1234'
   }
 
-  const encodedUrl = encodeUrlAndTransforms(expectedId, 'pos=10,20&w=100&h=200&fm=101&q=1234')
+  const encodedUrl = encodeUrlAndTransforms(expectedId, 'pos=10,20&w=100&h=200&fm=101&q=1234', '.jpg')
   
   const response = decodeBase64Params(encodedUrl)
   
